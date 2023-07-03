03fecab1b3b3d9d73de24622823551626d8f0da7
martonroux <marton.roux@epitech.eu>
2023-07-03 16:18:07 +0200

    [BACK] [FIX] changed CORS allowed adresses

--- back/main.py
+++ back/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://192.168.1.23:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
