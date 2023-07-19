7cb373cd87647c7176ac076eaa747663bf34030b
martonroux <marton.roux@epitech.eu>
2023-07-19 18:46:53 +0200

    [BACK] [ADD] put requests to add new workouts and delete workouts

--- back/api_requests/put/put_workout.py
+++ back/api_requests/put/put_workout.py
@@ -8,6 +8,10 @@ class WorkoutUpdate(BaseModel):
     workout: dict
 
 
+class WorkoutId(BaseModel):
+    id: int
+
+
 def put_workout(app):
     @app.put("/api/put/workout/update_workout")
     def put_data(workout_update: WorkoutUpdate = None):
@@ -25,5 +29,46 @@ def put_workout(app):
                 break
 
         if found is False:
-            data['workouts'].append(workout)
+            raise HTTPException(status_code=400, detail="Workout doesn't exist yet, or ID was changed (forbidden)")
+        write_data(data)
+
+    @app.put("/api/put/workout/add_workout")
+    def put_data(new_workout: WorkoutUpdate = None):
+        if new_workout is None:
+            raise HTTPException(status_code=400, detail="MUST provide workout data")
+
+        workout = new_workout.workout
+        wrkt_id = workout['id']
+        data = load_data()
+        found = False
+
+        for i in range(len(data['workouts'])):
+            if data['workouts'][i]['id'] == wrkt_id:
+                found = True
+                break
+
+        if found is True:
+            raise HTTPException(status_code=400, detail="Workout already exist with same ID.")
+
+        data['workouts'].append(workout)
+        write_data(data)
+
+    @app.put("/api/put/workout/delete_workout")
+    def put_data(workout_id: WorkoutId = None):
+        if workout_id is None:
+            raise HTTPException(status_code=400, detail="MUST provide workout ID")
+
+        id = workout_id.id
+        data = load_data()
+        found = False
+
+        for i in range(len(data['workouts'])):
+            if data['workouts'][i]['id'] == id:
+                data['workouts'].remove(data['workouts'][i])
+                found = True
+                break
+
+        if found is False:
+            raise HTTPException(status_code=400, detail="Workout with corresponding ID was not found")
+
         write_data(data)
