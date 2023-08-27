34d46d88de11af47029c588046b9fa8df23a7837
martonroux <marton.roux@epitech.eu>
2023-08-28 00:23:54 +0200

    [BACK] [ADD] put request for Quiz Janka

--- back/api_requests/put/janka_quiz.py
+++ back/api_requests/put/janka_quiz.py
@@ -20,10 +20,17 @@ class Quiz(BaseModel):
 def put_janka_quiz(app):
     @app.put("/update-janka-quiz/")
     async def update_janka_quiz(quiz_data: Quiz):
-        absolute_path = "/home/ubuntu/janka-quiz/dist/quiz.json"
+        absolute_path_dist = "/home/ubuntu/janka-quiz/dist/quiz.json"
+        absolute_path_pub = "/home/ubuntu/janka-quiz/public/quiz.json"
 
         try:
-            with open(absolute_path, "w") as f:
+            with open(absolute_path_dist, "w") as f:
+                json.dump(quiz_data.dict(), f)
+        except Exception as e:
+            return {"error": str(e)}
+
+        try:
+            with open(absolute_path_pub, "w") as f:
                 json.dump(quiz_data.dict(), f)
             return {"message": "Quiz updated successfully"}
         except Exception as e:
